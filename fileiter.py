9749b175422198acda0ef9ce5b751a8740fbf24d
Tianyi6679 <timtianyima@gmail.com>
2019-11-12 22:12:39 -0800

    implement file splitter that splits input file into multiple similar-sized smaller files

--- fileiter.py
+++ fileiter.py
@@ -1,11 +1,44 @@
 from itertools import islice
-
+import optparse
+import os
 
 
 def read(filename, n=20):
-    with open(filename, 'r') as f:
+    with open(filename, 'r', encoding="utf8") as f:
         while True:
-            lines =  ''.join([line for line in islice(f, n)])
+            lines = ''.join([line for line in islice(f, n)])
             if lines == '':
                 break
             yield lines
+
+
+def split_file(filename, n=4):
+    f = read(filename)
+    dirname = "./%s_split_%d" % (filename, n)
+    try:
+        os.mkdir(dirname)
+    except FileExistsError:
+        print("file already processed, please delete the directory before resplit")
+        return
+    files = [open("%s/%s_%d" % (dirname, filename, i), 'w') for i in range(n)]
+    for i, line in enumerate(f):
+        files[i % n].write(line)
+    for f in files:
+        f.close()
+
+
+def splitter_options_parse():
+    parser = optparse.OptionParser(usage='%prog [options]', version='%%prog %s' % '0.0.1')
+    parser.add_option('-f', '--filename', dest='filename', default="", help='name of input file')
+    parser.add_option('-n', '--number_of_splits', dest='number_of_splits', default='4',
+                      help='number of splits')
+    return parser
+
+
+if __name__ == '__main__':
+    parser = splitter_options_parse()
+    (options, args) = parser.parse_args()
+    if options.filename == "":
+        print("please enter file name")
+    else:
+        split_file(options.filename, int(options.number_of_splits))

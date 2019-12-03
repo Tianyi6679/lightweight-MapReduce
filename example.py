8d0bbe457de759c41df0fbee7a2f7938dc72cc9b
kelly zhang <kellyzhang@kellys-MacBook-Pro.local>
2019-12-02 18:37:58 -0800

    fix cache system

--- example.py
+++ example.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import distmm
 
-data = ['1342-0.txt_split_4\\1342-0.txt_0',
-        '1342-0.txt_split_4\\1342-0.txt_1',
-        '1342-0.txt_split_4\\1342-0.txt_2',
-        '1342-0.txt_split_4\\1342-0.txt_3']
+data = ['mac/1342-0.txt_split_4/1342-0.txt_0',
+        'mac/1342-0.txt_split_4/1342-0.txt_1',
+        'mac/1342-0.txt_split_4/1342-0.txt_2',
+        'mac/1342-0.txt_split_4/1342-0.txt_3']
 # The data source can be any dictionary-like object
 datasource = dict(enumerate(data))
 
